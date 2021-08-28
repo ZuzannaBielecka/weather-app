7d3414380f96ad634100c62ba8ba04c6ae36993f
Zuzanna <zuzia.bielecka@gmail.com>
2021-08-28 21:10:15 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
-function formatDate(date){
-
-  
+function formatDate(date) {
   let hour = date.getHours();
-  if (hour <10) {
+  if (hour < 10) {
     hour = `0${date.getHours()}`;
   }
   let minutes = date.getMinutes();
-  if (minutes <10) {
-    minutes =`0${date.getMinutes()}`;
+  if (minutes < 10) {
+    minutes = `0${date.getMinutes()}`;
   }
   let days = [
     "Sunday",
@@ -16,69 +14,56 @@ function formatDate(date){
     "Wednsday",
     "Thursday",
     "Friday",
-    "Saturday"
+    "Saturday",
   ];
-  let day = days.[date.getDay()];
+  let day = days[date.getDay()];
   return `${day}, ${hour}:${minutes}`;
 }
 
-
 let now = new Date();
-let dateDisplayed = document.querySelector('#current-day');
+let dateDisplayed = document.querySelector("#current-day");
 dateDisplayed.innerHTML = formatDate(now);
 
-function displayWeatherConditions (response) {
-  document.querySelector('#current-city').innerHTML = response.data.name;
-  document.querySelector('#current-temperature').innerHTML = Math.round(response.data.main.temp);
-  document.querySelector('.humidity').innerHTML = Math.round(response.data.main.humidity);
-  document.querySelector('.wind').innerHTML = Math.round(response.data.wind.speed);
-  document.querySelector('#description').innerHTML = (response.data.weather[0].main);
-
-  
+function displayWeatherConditions(response) {
+  document.querySelector("#current-city").innerHTML = response.data.name;
+  document.querySelector("#current-temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector(".humidity").innerHTML = Math.round(
+    response.data.main.humidity
+  );
+  document.querySelector(".wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+  document.querySelector("#description").innerHTML =
+    response.data.weather[0].main;
 }
-function search (city){
+function search(city) {
   let key = "d258a15b9cfd02450a6bca5a0ce192b2";
   let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=metric`;
-  
+
   axios.get(url).then(displayWeatherConditions);
-
-
 }
 function submitCity(event) {
   event.preventDefault();
-  let city = document.querySelector('#city-input').value;
+  let city = document.querySelector("#city-input").value;
   search(city);
- 
-
 }
 
-let searchButton = document.querySelector('#search-button');
+let searchButton = document.querySelector("#search-button");
 searchButton.addEventListener("click", submitCity);
 
-
-
-
-function handlePosition(response){
- let lat = response.coords.latitude;
- let long = response.coords.longitude;
- let key = "d258a15b9cfd02450a6bca5a0ce192b2";
- let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${key}&units=metric`;
- console.log(url);
- axios.get(url).then(displayWeatherConditions);
-
-
+function handlePosition(response) {
+  let lat = response.coords.latitude;
+  let long = response.coords.longitude;
+  let key = "d258a15b9cfd02450a6bca5a0ce192b2";
+  let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${key}&units=metric`;
+  console.log(url);
+  axios.get(url).then(displayWeatherConditions);
 }
-let currentButton = document.querySelector('#current-location-button');
-currentButton.addEventListener("click", navigator.geolocation.getCurrentPosition(handlePosition));
-;
-
+let currentButton = document.querySelector("#current-location-button");
+currentButton.addEventListener(
+  "click",
+  navigator.geolocation.getCurrentPosition(handlePosition)
+);
 search("New York");
-
-
-
-
-
-
-
-
-
